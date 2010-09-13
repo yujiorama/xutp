12d689c37804ad1c93b0f12f62de018e3a9c33c9
yujiorama <yujiorama+github@gmail.com>
2010-09-13 23:21:51 +0900

    リファクタリングしたやつを追加

--- chapter24_parameterized_test_sample.rb
+++ chapter24_parameterized_test_sample.rb
@@ -32,3 +32,34 @@ def testTestterm_plural
                       "testterms: html output")
 end
 
+def test_extref
+  sourceXml = "<extref id='abc' />"
+  expectedHtml = "<a href='abc.html'>abc</a>"
+  generateAndVerifyHtml(sourceXml, expectedHtml, "<extref>")
+end
+
+def test_testterm_normal
+  sourceXml = "<testterm id='abc' />"
+  expectedHtml = "<a href='abc.html'>abc</a>"
+  generateAndVerifyHtml(sourceXml, expectedHtml, "<testterm>")
+end
+
+def test_testterm_plural
+  sourceXml = "<testterms id='abc' />"
+  expectedHtml = "<a href='abc.html'>abc</a>"
+  generateAndVerifyHtml(sourceXml, expectedHtml, "<plural>")
+end
+
+def generateAndVerifyHtml( sourceXml, expectedHtml,
+                           message, &block)
+  mockFile = MockFile.new
+  sourceXml.delete!("\t")
+  @handler = setupHandler(sourceXml, mockFile)
+  block.call unless block == nil
+  @handler.printBodyContents
+  actual_html = mockFile.output
+  assert_equals_html( expectedHtml,
+                      actual_html,
+                      message + "html output")
+  actual_html
+end
